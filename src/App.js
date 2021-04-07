86561e15c0566d419289c764b4abaa26052fadc8
Mike Cao <mikecao369@gmail.com>
2021-04-06 22:34:48 -0400

    update react component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    students: []
+  }
+
+  async componentDidMount() {
+    const response = await fetch('https://api.hatchways.io/assessment/students')
+    const data = await response.json();
+    this.setState({ students: data.students})
+  }
+
+  render() {
+      let sum = 0
+    return (
+      <div className="App">
+        {this.state.students.map((student, idx) => {
+          return (
+            <div>
+              <img src={student.pic} alt='student pic' key={idx}/>
+              <h3>{student.firstName} {student.lastName}</h3>
+              <p>Email: {student.email}</p>
+              <p>Company: {student.company}</p>
+              <p>Skill: {student.skill}</p>
+              <p>
+                Average: {student.grades.map((grade) => {
+                  sum += grade;
+                  return sum
+                })}
+              </p>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 }
 
 export default App;
